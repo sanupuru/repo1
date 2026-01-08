9172385064e458c14df8378bdaed2fb80a65c7cd
sanupuru <seshdeals@gmail.com>
2026-01-08 17:58:22 +0000

    updates

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = var.sesh-ami
   instance_type = var.vm-size
-  # adding security group
+  # attaching security group to ec2 vm 
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   tags = {
     Name = var.VMName
